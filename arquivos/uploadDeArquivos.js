4b36b8376fb274c40786248932745acbf61da345
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-06-28 01:07:50 -0300

    criando clientes

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,18 +1,20 @@
-const fs = require('fs')
+const fs = require("fs");
+const path = require("path");
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+  const tiposValidos = ["jpg", "png", "jpeg"];
+  const tipo = path.extname(caminho);
+  const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) != -1;
 
-fs.readFile('./assets/chow-chow.jpg', (erro, buffer) => {
-    if(erro){
-        console.log(erro)
-    }else{
-        console.log('A imagem foi bufferizada!')
-        console.log(buffer)
-
-        fs.writeFile('./assets/chow-chow2.jpg', buffer, (erro, buffer) => {
-            if(erro){
-                console.log(erro)
-            }else{
-                console.log("Imagem 2 escrita com sucesso!")
-            }
-        })
-    }
-})
+  if (tipoEhValido) {
+      const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`;
+      fs.createReadStream(caminho)
+      .pipe(fs.createWriteStream(novoCaminho))
+      .on("finish", () => {
+          callbackImagemCriada(false, novoCaminho);
+        });
+    } else {
+        const erro = "Tipo é inválido!"
+        callbackImagemCriada(erro)
+      console.log("Erro! tipo inválido");
+  }
+};
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 class Atendimento {
@@ -55,11 +56,16 @@ class Atendimento {
     buscaPorId(id,res){
         const sql = `SELECT * FROM Atendimentos WHERE id=${id}`
 
-        conexao.query(sql, (erro, resultado) => {
+        conexao.query(sql, async (erro, resultado) => {
             const atendimento = resultado[0]
+            const cpf = atendimento.cliente
             if(erro){
                 res.status(400).json(erro)
             }else{
+                const { data } = await axios.get(`http://localhost:8082/${cpf}`)
+
+                atendimento.cliente = data
+
                 res.status(200).json(atendimento)
             }
         })
